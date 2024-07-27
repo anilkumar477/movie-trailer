bfe1c741b3de5d0314127005ff2ee2c7a5c5a21b
anil kumar nodagala <anilnodagala.123@gmail.com>
2024-07-27 12:07:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import ReactPlayer from 'react-player';
+import movieTrailer from 'movie-trailer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+  const [video,setVideo] = useState('fordvferrari')
+  const [videoUrl,setVideoUrl] = useState("https://youtu.be/xAkCqox_MhE")
+  
+  function handleClickSearch(){
+    movieTrailer(video).then((res) =>{
+      setVideoUrl(res);
+    })
+  }
+  return(
+    <div className='bg-container'>
+      <div>
+        <h1 className='header'>Search your favorite movie Trailers</h1>
+      <div className='search-bar'>
+        <label className='label-text' >Search for any movies : {' '}</label>
+        <input  
+        type='text' 
+        className='input-text'
+        onChange={(e)=>{
+          setVideo(e.target.value)
+        }}
+        
+        />
+        <button className='button' onClick={handleClickSearch}>Search</button>
+
+      </div>
+      <div className='player'>
+      <ReactPlayer url={videoUrl} controls={true}/>
     </div>
-  );
+    </div>
+    </div>
+  )
 }
-
-export default App;
